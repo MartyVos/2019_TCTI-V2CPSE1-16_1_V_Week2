d4c6e8c5ce22a8e5b106da54735d4f2af8f1016a
MartyVos <marty.vos@student.hu.nl>
2019-09-20 15:28:53 +0200

    Added assignment 5

---
+++ 5_Maakfile/generator.cpp
@@ -0,0 +1,37 @@
+#include "note_player_native.hpp"
+#include "rtttl_player.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+const char sos[]         	= "SOS:d=4,o=5,b=60:a,p,a,p,a,p,2a.,p,2a.,p,2a.,p,a,p,a,p,a";
+const char let_it_be[]   	= "Beatles Let It Be:d=4,o=5,b=100:16e6,8d6,c6,16e6,8g6,8a6,8g.6,16g6,8g6,8e6,16d6,8c6,16a,8g,e.6,p,8e6,16e6,8f.6,8e6,8e6,8d6,16p,16e6,16d6,8d6,2c.6";
+const char muppets[]     	= "Muppets:d=4,o=5,b=250:c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,8a,8p,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,8e,8p,8e,g,2p,c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,a,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,d,8d,c";
+const char rickroll[]    	= "Never gonna give you up:d=4,o=5,b=120:16c,16d,16f,16d,16a.,16p,32p,8a,16p,g.,16c,16d,16f,16d,16g.,16p,32p,8g,16p,8f.,16e,8d,16c,16d,16f,16d,f,8g,8e.,16d,8c,8c4,8c,8g,8p,2f,16c,16d,16f,16d,16a.,16p,32p,8a,16p,g.,16c,16d,16f,16d,c6,8e,8f.,16e,8d,16c,16d,16f,16d,f,8g,8e.,16d,8c,8c4,8c,8g,8p,2f";
+const char one[]         	= "NumberOne:d=16,o=5,b=168:4f.,8c6,16b5,16c6,16b5,16c6,8b5,8c6,4g#5,4f.,8f,8g#5,8c6,4c#6,4g#5,4c#6,4d#6,8c6,8c#6,8c6,8c#6,2c6";
+const char imperial[]	  	= "Imperial:d=4,o=5,b=80:8d.,8d.,8d.,8a#4,16f,8d.,8a#4,16f,d.,32p,8a.,8a.,8a.,8a#,16f,8c#.,8a#4,16f,d.,32p,8d.6,8d,16d,8d6,32p,8c#6,16c6,16b,16a#,8b,32p,16d#,8g#,32p,8g,16f#,16f,16e,8f,32p,16a#4,8c#,32p,8a#4,16c#,8f.,8d,16f,a.,32p,8d.6,8d,16d,8d6,32p,8c#6,16c6,16b,16a#,8b,32p,16d#,8g#";
+const char indiana[]	  	= "IndianaJones:d=4,o=5,b=250:e,8p,8f,8g,8p,2c.6,8p.,d,8p,8e,1f,p.,g,8p,8a,8b,8p,2f.6,p,a,8p,8b,2c6,2d6,2e6,e,8p,8f,8g,8p,1c6,p,d6,8p,8e6,1f6,g,8p,8g,e.6,8p,d6,8p,8g,e.6,8p,d6,8p,8g,f.6,8p,e6,8p,8d6,1c6";
+
+void export_song_to_file(note_player &p, const char *song, std::ofstream &file){
+	file.open("melody.cpp");
+	file
+		<< "#include \"melody.hpp\"\n\n"
+		<< "class song : public melody{\n"
+		<< "public:\n"
+		<< "\tvoid play(note_player &p){\n";
+
+	rtttl_play(p, song);
+	
+	file
+		<< "\t}\n"
+		<< "};"
+		;
+	file.close();
+}
+
+
+int main( void ){
+	std::ofstream file;
+	auto p = note_player_native(file);
+	export_song_to_file(p, imperial, file);
+}
